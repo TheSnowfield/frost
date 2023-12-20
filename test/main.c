9e1d4c08950e413b82859dd842582c7bad047fe4
TheSnowfield <17957399+TheSnowfield@users.noreply.github.com>
2023-12-21 05:12:12 +0800

    other: stage files

---
+++ test/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define FROST_PROTED_TICK_TIME
+#define FROST_PROTED_LOG_PRINT
+#include <frost/api.h>
+
+void __callback_test() {
+  printf("=w=");
+}
+
+int main() {
+
+  frost_init();
+  frost_task_interval(&__callback_test, 1000, NULL);
+
+  // initialization okay, run tasks
+  frost_errcode_t _result = frost_err_ok;
+  while (frost_ok(_result, _result = frost_schedule_tasks())) {
+    // schedule the tasks
+  }
+
+}
