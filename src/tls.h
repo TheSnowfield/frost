5ff1f3790288871e7cb8045f05d04c14a76d06c3
TheSnowfield <17957399+TheSnowfield@users.noreply.github.com>
2025-09-23 17:33:22 +0800

    fix: fix symbol definition & append usage of tls functions

--- src/tls.h
+++ src/tls.h
@@ -7,23 +7,23 @@
 /**
  * @brief create task local storage
  *
- * @param task current task context pointer, if NULL, get current task context
- * @return frost_errcode_t if success return orinus_ok
+ * @param task task context pointer, pass NULL meant to use current task context
+ * @return frost_errcode_t if success return frost_err_ok
  */
 frost_errcode_t tls_alloc_ex(frost_task_ctx_t* task);
 
 /**
  * @brief destroy task local storage
  *
- * @param task current task context pointer, if NULL, get current task context
- * @return frost_errcode_t if success return orinus_ok
+ * @param task task context pointer, pass NULL meant to use current task context
+ * @return frost_errcode_t if success return frost_err_ok
  */
 frost_errcode_t tls_destroy_ex(frost_task_ctx_t* task);
 
 /**
  * @brief check task local storage is allocated
  *
- * @param task current task context pointer, if NULL, get current task context
+ * @param task task context pointer, pass NULL meant to use current task context
  * @return true / false
  */
 bool tls_is_allocated_ex(frost_task_ctx_t* task);
@@ -31,29 +31,62 @@ bool tls_is_allocated_ex(frost_task_ctx_t* task);
 /**
  * @brief put value to task local storage
  *
- * @param task current task context pointer, if NULL, get current task context
+ * @param task task context pointer, pass NULL meant to use current task context
  * @param index key index
  * @param value value
  *
- * @return frost_errcode_t if success return orinus_ok
+ * @return frost_errcode_t if success return frost_err_ok
  */
 frost_errcode_t tls_set_value_ex(frost_task_ctx_t* task, uint32_t index, size_t value);
 
 /**
  * @brief get value from task local storage
  *
- * @param task current task context pointer, if NULL, get current task context
+ * @param task task context pointer, pass NULL meant to use current task context
  * @param index key index
  * @param value value
  *
- * @return frost_errcode_t if success return orinus_ok
+ * @return frost_errcode_t if success return frost_err_ok
  */
 frost_errcode_t tls_get_value_ex(frost_task_ctx_t* task, uint32_t index, size_t* value);
 
+/**
+ * @brief allocate task local storage of the task
+ *
+ * @return frost_errcode_t if success return frost_err_ok
+ */
 frost_errcode_t tls_alloc();
+
+/**
+ * @brief destroy the task local storage
+ *
+ * @return frost_errcode_t if success return frost_err_ok
+ */
 frost_errcode_t tls_destroy();
-bool tls_is_allocated();
+
+/**
+ * @brief is task local storage allocated
+ *
+ * @param task task context pointer, pass NULL meant to use current task context
+ */
+bool tls_is_allocated(frost_task_ctx_t* task);
+
+/**
+ * @brief set value from task local storage
+ *
+ * @param index index
+ * @param value value
+ * @return frost_errcode_t
+ */
 frost_errcode_t tls_set_value(uint32_t index, size_t value);
+
+/**
+ * @brief get value from task local storage
+ *
+ * @param index index
+ * @param value value
+ * @return frost_errcode_t
+ */
 frost_errcode_t tls_get_value(uint32_t index, size_t* value);
 
 #endif /* _FROST_TLS_H */
