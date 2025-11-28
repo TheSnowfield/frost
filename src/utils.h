26f298273c115ded020a133e5436c03e8938eaa6
TheSnowfield <17957399+TheSnowfield@users.noreply.github.com>
2025-11-28 20:36:36 +0800

    utils: move __get_task_ctx here

---
+++ src/utils.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+/*******************************************************************************
+ * This file is the part of the Frost library
+ *
+ * (C) Copyright 2025 TheSnowfield.
+ *
+ * Authors: TheSnowfield <17957399+TheSnowfield@users.noreply.github.com>
+ ****************************************************************************/
+
+static frost_task_ctx_t* __get_task_ctx(frost_task_ctx_t* task) {
+  
+  if(task != NULL) return task;
+
+  frost_task_ctx_t* _task;
+  frost_errcode_t _result;
+
+  // get current task context
+  if(!frost_ok(_result = frost_task_get_context(&_task))) {
+    frost_log(TAG, "invalid task context");
+    return NULL;
+  }
+
+  return _task;
+}
